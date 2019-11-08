2a3ec301e3ccdefacf42b41caa197fea81463d65
Ibrahim Yunus <younis.ib@gmail.com>
2019-11-07 22:53:12 -0500

    cleanup

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -98,7 +98,7 @@ class Game extends Component {
         });
 
         // removes the green correct indicator on a successful click after .5s to re-render the class on each success
-        return setTimeout(() => this.setState({ navMsgColor: '' }), 500);
+        return setTimeout(() => this.setState({ messageColor: '' }), 500);
     }
 
     // renders score to the navbar.
@@ -126,104 +126,5 @@ class Game extends Component {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Game extends Component {
-
-//     // set a state
-//     state = {
-//         friendArray: friends,
-//         clickedId: []
-//     }
-
-//     handleImageClick = (id) => {
-
-//         let isClicked = false;
-
-//         //const isClicked = friends.find((friend) => (friend.id === id && friend.isClicked));
-
-
-//         // if (typeof isClicked !== "object") {
-
-//         const newFriends = friends.map((friend) => {
-
-//             if (!this.state.clickedId.includes(friend.id) && friend.id === id) {
-
-//                 console.log(friend);
-
-//                 return {
-//                     ...friend, isClicked: true
-//                 }
-//             } else if (friend.id !== id) {
-
-//                 return friend;
-
-//             } else {
-
-//                 console.log(isClicked);
-//                 isClicked = true;
-
-//                 return {};
-//             }
-//         });
-
-
-//         if (isClicked) {
-
-//             console.log("lost game");
-
-//         } else {
-
-//             let newIds = [
-
-//                 ...this.state.clickedId, id
-//             ];
-
-//             this.setState({
-
-//                 friendArray: newFriends,
-//                 clickedId: newIds
-//             });
-//         }
-
-//         //  } else {
-
-//         //      console.log("lost game");
-//         //  }
-
-//     }
-
-//     componentDidUpdate() {
-
-//         console.log(this.state);
-//     }
-//     render() {
-
-//         return (
-
-//             <div>
-//                 {
-//                     friends.map((friend) => (
-//                         <img className="imgWidth" src={friend.image} alt={friend.name} key={friend.id} onClick={() => this.handleImageClick(friend.id)} isclicked={`${friend.isClicked}`} />
-//                     ))
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 export default Game;
